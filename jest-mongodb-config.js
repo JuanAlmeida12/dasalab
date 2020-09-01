23ae55ca0e7832610916c95a7dd826623986fcf4
Juan <juan.satyro@gmail.com>
2020-09-01 10:39:13 -0300

    Add test configurations

---
+++ jest-mongodb-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongodbMemoryServerOptions: {
+      instance: { },
+      binary: {
+        version: '4.2.3', // Version of MongoDB
+        skipMD5: true
+      },
+      autoStart: false
+    }
+  }
--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-const mongodbUrl = process.env.MONGODB || 'mongodb://localhost:27017/dasa'
+const mongodbUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/dasa'
 
 mongoose.connect(mongodbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
