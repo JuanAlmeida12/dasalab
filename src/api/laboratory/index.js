7f0a533bc769c5ae066c30a0ec3bff0067eb8dee
Juan Almeida <juan.satyro@gmail.com>
2020-09-03 03:26:56 -0300

    Add laboratory api layer

---
+++ src/api/laboratory/index.js
@@ -0,0 +1,70 @@
+/**
+ * Sets the base URL to this API
+ */
+const LABORATORY_BASE_URL = '/laboratories'
+
+const laboratoryDB = require('../../data-access/laboratory-db')
+
+module.exports = (router, database) => {
+    /**
+       * Retrieves all laboratories from database
+       */
+    router.get(LABORATORY_BASE_URL, (req, res) => {
+        // Returning Status 200 and the json with laboratories.
+        laboratoryDB.listLaboratories().then(data => {
+            res.status(200).json(data)
+        }).catch(err => {
+            res.status(400).json({ message: err.message })
+        })
+    })
+
+    /**
+       * Add new laboratory on persistence
+       */
+    router.post(LABORATORY_BASE_URL, (req, res) => {
+        laboratoryDB.addLaboratory(req.body).then(data => {
+            res.status(201).json(data)
+        }).catch(err => {
+            res.status(400).json({ message: err.message })
+        })
+    })
+
+    /**
+       * Retrieves a single laboratory
+       */
+    router.get(`${LABORATORY_BASE_URL}/:id`, (req, res) => {
+        const { id } = req.params
+
+        laboratoryDB.findLaboratory(id).then(data => {
+            res.status(200).json(data)
+        }).catch(err => {
+            res.status(204).json(err)
+        })
+    })
+
+    /**
+       * Update laboratory on persistence
+       */
+    router.put(`${LABORATORY_BASE_URL}/:id`, (req, res) => {
+        const { id } = req.params
+
+        laboratoryDB.updateLaboratory(id, req.body).then(data => {
+            res.status(200).json(data)
+        }).catch(err => {
+            res.status(400).json(err)
+        })
+    })
+
+    /**
+       * Delete laboratory on persistence
+       */
+    router.delete(`${LABORATORY_BASE_URL}/:id`, (req, res) => {
+        const { id } = req.params
+
+        laboratoryDB.deleteLaboratory(id).then(data => {
+            res.status(204).json(data)
+        }).catch(err => {
+            res.status(400).json(err)
+        })
+    })
+}
